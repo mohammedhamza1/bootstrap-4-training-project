8629103a9cdbf42621fce2699fa637b139694950
Mohammed Hamza <mohammedhamza1@users.noreply.github.com>
2017-10-06 20:56:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,36 +3,34 @@ $(document).ready(function () {
     'use strickt';
 
     // add slider height
-
     var windowHeight = $(window).height(),
         upperHeight = $('.upper-bar').innerHeight(),
         navHeight = $('.navbar').innerHeight();
 
     $('.slider,.carousel-item').height(windowHeight - (upperHeight + navHeight));
 
-    // trigger the carousel
-
-
-
-    // centering slider heading
+    // centering slider heading in slider section
     $('.slider .overlay h1').centertop();
-    // centering fontawesome in overlay
+    // centering fontawesome in featured work section
     $('.our-work .shuffle-images .col-sm .overlay i').centerany();
-
-
 
     // shuffle images section
     $('.our-work .our-feilds span').on('click', function () {
+        // make controller active
         $(this).addClass('active').siblings().removeClass('active');
 
         if ($(this).data('class') === 'all') {
+            // display all imgs
             $('.our-work .shuffle-images .col-sm').css('opacity', '1');
         } else {
+            // hide all imgs except imgs that have controller data class
             $('.our-work .shuffle-images .col-sm').css('opacity', '.09');
             $('.' + $(this).data('class')).css('opacity', '1');
 
+
+
+
         }
     });
-
 
 });
